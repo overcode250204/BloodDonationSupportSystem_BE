9cbecc752cf09cfbae6fa5e9a5100449fc7d6ee0
MinhDuc <phuongngaduc@gmail.com>
2025-07-10 03:14:44 +0700

    fix userprofile without email

--- src/main/java/com/example/BloodDonationSupportSystem/service/userservice/UserService.java
+++ src/main/java/com/example/BloodDonationSupportSystem/service/userservice/UserService.java
@@ -20,6 +20,7 @@ public class UserService {
 
     @Autowired
     private SearchDistanceService searchDistanceService;
+
     public UserProfileDTO getCurrentUserProfile() {
         try {
             UserDetails currentUser = AuthUtils.getCurrentUser();
@@ -75,7 +76,11 @@ public class UserService {
         userProfileDTO.setLongitude(userEntity.getLongitude());
         userProfileDTO.setLatitude(userEntity.getLatitude());
         userProfileDTO.setRole(userEntity.getRole().getRoleName());
-        userProfileDTO.setEmail(userEntity.getOauthAccount().getAccount());
+        if (userEntity.getOauthAccount() != null) {
+            userProfileDTO.setEmail(userEntity.getOauthAccount().getAccount());
+        } else {
+            userProfileDTO.setEmail(null);
+        }
         return userProfileDTO;
     }
 
