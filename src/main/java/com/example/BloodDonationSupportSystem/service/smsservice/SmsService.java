da13199c0bc3c9ecd7ae20b1eb1b9d1e9d208697
Overcode2502 <saoran123456@gmail.com>
2025-07-11 13:02:39 +0700

    addFailToRegistration

--- src/main/java/com/example/BloodDonationSupportSystem/service/smsservice/SmsService.java
+++ src/main/java/com/example/BloodDonationSupportSystem/service/smsservice/SmsService.java
@@ -18,9 +18,9 @@ public class SmsService {
     @Value("${device}")
     private String device;
 
-    public String sendSmsHealthReminder(String phoneNumber, String username) {
+    public String sendSmsHealthReminder(String phoneNumber, String name) {
         try {
-            String message = "Xin chào " + username + ",\n\n" +
+            String message = "Xin chào " + name + ",\n\n" +
                     "Cảm ơn bạn đã tham gia hiến máu tình nguyện.\n\n" +
                     "Vui lòng theo dõi tình trạng sức khỏe trong 48 giờ tới. " +
                     "Nếu có dấu hiệu bất thường như chóng mặt, mệt mỏi kéo dài, hãy đến trung tâm y tế gần nhất.\n\n" +
@@ -37,10 +37,10 @@ public class SmsService {
 
     }
 
-    public String sendSmsSuccessRegistrationNotification(String phoneNumber, String username, String dateTime, String location) {
+    public String sendSmsSuccessRegistrationNotification(String phoneNumber, String name, String dateTime, String location) {
 
         try {
-            String message = "Xin chào " + username + ",\n\n" +
+            String message = "Xin chào " + name + ",\n\n" +
                     "Chúc mừng bạn đã đăng ký hiến máu thành công!\n\n" +
                     "Thời gian hiến máu: " + dateTime + "\n" +
                     "Địa điểm hiến máu: " + location + "\n\n" +
@@ -61,8 +61,27 @@ public class SmsService {
         }
 
 
+
     }
 
+    public String sendRegistrationFailureNotification(String phoneNumber) {
+        try {
+            String message = "Chúng tôi rất tiếc phải thông báo rằng đơn đăng ký hiến máu của bạn không thành công " +
+                    "vì hiện tại không có lịch hiến máu nào phù hợp với khoảng thời gian mà bạn đã chọn.\n\n" +
+                    "Bạn có thể truy cập hệ thống để đăng ký lại với một khoảng thời gian khác hoặc chờ đợi khi có lịch mới được cập nhật.\n\n" +
+                    "Cảm ơn bạn đã quan tâm và sẵn sàng đóng góp để giúp đỡ những người cần máu. " +
+                    "Chúng tôi hy vọng sẽ sớm đồng hành cùng bạn trong các chương trình hiến máu tiếp theo.\n\n" +
+                    "Nếu có bất kỳ thắc mắc hoặc cần hỗ trợ, vui lòng liên hệ:\n" +
+
+                    "Số điện thoại: " + phoneNumber + "\n\n" +
+                    "Trân trọng,\n" +
+                    "Trung tâm Hiến máu";
+            return speedSMSUtils.sendSMS(phoneNumber, message, 2, device);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Send SMS fail" + e.getMessage();
+        }
+    }
 
 
 
